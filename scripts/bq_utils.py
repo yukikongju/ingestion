387e11fd9de1060f5ee0af20474fbc8b4c104677
yukikongju <emulie.chhor@hotmail.com>
2025-09-19 12:16:04 -0400

    added script to get bq table name full path based on branch

---
+++ scripts/bq_utils.py
@@ -0,0 +1,26 @@
+from git import Repo
+
+def get_current_branch(path=".") -> str:
+    repo = Repo(path)
+    return repo.active_branch.name
+
+def get_env_name_from_branch() -> str:
+    branch = get_current_branch()
+    if branch == "main":
+        return "prod"
+    elif branch == "staging":
+        return "staging"
+    else:
+        return "dev"
+
+
+def get_bigquery_table_path(project_id: str, dataset_id: str, table_id: str) -> str:
+    """
+    Get bigquery table path based on branch
+
+    Example
+    -------
+    > get_bigquery_table_path('relax-melodies-android', 'ua_extraction', 'facebook_ads')
+    """
+    env = get_env_name_from_branch()
+    return f"{project_id}.{dataset_id}_{env}.{table_id}"
